1ecd1ee9f441f77d857900905e7e35464f003622
justinewalt <justinewalt@gmail.com>
2016-06-01 14:16:40 -0600

    updated gemfile and continued with setup process:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= materialize-sprockets
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
